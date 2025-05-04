113e2a35e56acc8605e5da391eeba05af6c05ed5
motoshifurugen <amanogawa_m@icloud.com>
2025-05-04 16:42:22 +0900

    #16 componentsディレクトリの再構成

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -2,9 +2,9 @@ import { Tabs } from 'expo-router';
 import React from 'react';
 import { Platform } from 'react-native';
 
-import { HapticTab } from '@/components/HapticTab';
-import { IconSymbol } from '@/components/ui/IconSymbol';
-import TabBarBackground from '@/components/ui/TabBarBackground';
+import { HapticTab } from '@/components/common/HapticTab';
+import { IconSymbol } from '@/components/common/ui/IconSymbol';
+import TabBarBackground from '@/components/common/ui/TabBarBackground';
 import { Colors } from '@/constants/Colors';
 import { useColorScheme } from '@/hooks/useColorScheme';
 
--- app/(tabs)/home.tsx
+++ app/(tabs)/home.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { Image, StyleSheet, Platform } from 'react-native';
 
-import { HelloWave } from '@/components/HelloWave';
-import ParallaxScrollView from '@/components/ParallaxScrollView';
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
+import { HelloWave } from '@/components/common/HelloWave';
+import ParallaxScrollView from '@/components/common/ParallaxScrollView';
+import { ThemedText } from '@/components/common/ThemedText';
+import { ThemedView } from '@/components/common/ThemedView';
 
 export default function HomeScreen() {
   return (
